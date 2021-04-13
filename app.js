68cf22d46f0485bd6a07e2bf237bac3551ffe525
Kolade <chrisjay967@gmail.com>
2021-04-13 11:11:56 +0200

    changed Mr. Ebuka -D

--- app.js
+++ app.js
@@ -9,13 +9,9 @@ fs.mkdir("result", (err) => {
 fetch("http://jsonplaceholder.typicode.com/posts")
   .then((res) => res.json())
   .then((data) =>
-    fs.writeFile(
-      "./result/posts.json",
-      JSON.stringify(data, null, 2),
-      (err) => {
-        if (err) throw err;
-        console.log("Results Received");
-      }
-    )
+    fs.writeFile("./result/posts.txt", JSON.stringify(data, null, 2), (err) => {
+      if (err) throw err;
+      console.log("Results Received");
+    })
   )
   .catch((err) => console.log(err));
